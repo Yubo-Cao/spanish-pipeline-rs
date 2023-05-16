c647d89f432ade2970c796e6a035029c9fe6fdfc
Yubo-Cao <cao2006721@gmail.com>
2023-05-16 09:03:57 -0400

    chore(cargo): add clap

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ pub mod spider;
 
 use log::info;
 use simple_logger::SimpleLogger;
-use spider::google_image::image_search;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -13,9 +12,5 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .map(|()| log::set_max_level(log::LevelFilter::Info))?;
     info!(target: "main", "starting");
 
-    let images = image_search("cat", 100).await?;
-    for image in images {
-        println!("{}", image);
-    }
     Ok(())
 }
