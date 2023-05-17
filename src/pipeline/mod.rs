108c8479312af9714725ab97b04f7159146d0c94
Yubo-Cao <cao2006721@gmail.com>
2023-05-17 16:10:03 -0400

    feat: add pipeline error

--- src/pipeline/mod.rs
+++ src/pipeline/mod.rs
@@ -55,3 +55,18 @@ pub trait Pipeline {
         input: Option<PipelineIO>,
     ) -> Result<PipelineIO, Box<dyn std::error::Error>>;
 }
+
+/// Represents a Pipeline Error
+ 
+#[derive(Debug)]
+pub struct PipelineError {
+    message: String,
+}
+
+impl std::error::Error for PipelineError {}
+
+impl std::fmt::Display for PipelineError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
